791640b42071dfa661f3fc426a05008ee41ed05d
discountry <331027793@qq.com>
2017-03-15 13:37:22 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,1 @@
+console.log('Hello, webpack is using:', API_URL);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+// this config can be in webpack.config.js or other file with constants
+var API_URL = {
+  production: JSON.stringify('prod-url'),
+  development: JSON.stringify('dev-url')
+}
+
+module.exports = function(env) {
+  // check environment mode
+var environment = env === 'production' ? 'production' : 'development';
+  return {
+    entry: './app/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'API_URL': API_URL[environment]
+      })
+    ],
+  };
+}
+
